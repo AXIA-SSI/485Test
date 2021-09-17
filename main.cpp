a834644bffead5fbe71678db08ebca19e7efd875
chureee77 <chureee@hitel.net>
2021-09-17 19:19:38 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,55 +1,35 @@
-#include <QCoreApplication>
 #include <wiringSerial.h>
 #include <wiringPi.h>
 #include <stdio.h>
 #include <strings.h>
 #include <unistd.h>
-#include <QThread>
+
 
 #define EN_485 4
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    QCoreApplication a(argc, argv);
+    if(wiringPiSetupGpio() < 0) { //use BCM2835 Pin number table
+        printf("set wiringPi lib failed	!!! \r\n");
+        return 1;
+    } else {
+        printf("set wiringPi lib success  !!! \r\n");
+    }
+    pinMode(EN_485, OUTPUT);
+    digitalWrite(EN_485,HIGH);
 
-    {
-        if(wiringPiSetupGpio() < 0) { //use BCM2835 Pin number table
-            printf("set wiringPi lib failed	!!! \r\n");
-            return 1;
-        } else {
-            printf("set wiringPi lib success  !!! \r\n");
-        }
-        pinMode(EN_485, OUTPUT);
-        digitalWrite(EN_485,HIGH);
-
-        int fd;
-        if((fd = serialOpen ("/dev/ttyS0",9600)) < 0) {
-            printf("serial err\n");
-            return -1;
-        }
-
-        serialFlush(fd);
-        serialPrintf(fd,"\r");
-
-//        serialPuts(fd, "12345\n");
-//        serialPuts(fd, "56789\n");
-
-        printf("0\r\n");
-        QByteArray array;
-        array = QByteArray::fromHex("010600010100D99A");
-        //serialPuts(fd, array);
-        serialPuts(fd, "010600010100D99A");
-        printf("1\r\n");
-        QThread::sleep(1);
-        array = QByteArray::fromHex("010600010200D96A");
-        //serialPuts(fd, array);
-        serialPuts(fd, "010600010200D96A");
-        printf("2\r\n");
-
-        serialFlush(fd);
-        serialClose(fd);
-        printf("3\r\n");
+    int fd;
+    if((fd = serialOpen ("/dev/ttyS0",9600)) < 0) {
+        printf("serial err\n");
+        return -1;
     }
 
-    return a.exec();
+    serialFlush(fd);
+    serialPrintf(fd,"\r");
+
+    serialPuts(fd, "12345\n");
+    serialPuts(fd, "56789\n");
+
+    serialClose(fd);
+    return 0;
 }
